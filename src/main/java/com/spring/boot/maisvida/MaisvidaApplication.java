3dae85f681c110f31df7c5958e7ca72668eac117
Matheus <matheuscastro824@gmail.com>
2018-08-30 20:02:04 -0300

    Inicio Projeto

---
+++ src/main/java/com/spring/boot/maisvida/MaisvidaApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.maisvida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisvidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisvidaApplication.class, args);
+	}
+}
